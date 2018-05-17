dde390e7cc9e00f4657c70bdf1eea3091d60e805
Ankur Puri <apuri@iit.edu>
2018-05-17 22:37:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
+  
+  function onKeyDownHandler(e) {
+    const key = e.which;
+    
+    if(key === codes[index]) {
+      index++;
+      
+      if(index === codes.length) {
+        alert("Congratulations! You found an easter egg!");
+        index = 0;
+      }
+    } else {
+        index = 0;
+      }
+    
+  }
 }
